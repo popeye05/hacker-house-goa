fab84a2ab4f2451e3675167304f4406265b9bc4f
popeye05 <swarnab.popeye@outlook.com>
2026-08-16 22:19:06 +0530

    Final Setup

--- task2-ragingoa/backend/app/main.py
+++ task2-ragingoa/backend/app/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://rag-in-goa-nerdrats.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
